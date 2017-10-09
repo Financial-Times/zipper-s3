c4cf85c43f0e4afe6dc2097205c7964ead1b4aa4
David Balazs <david.balazs1993@ft.com>
2017-10-09 15:08:33 +0300

    Uncommented tests

--- storage_test.go
+++ storage_test.go
@@ -60,71 +60,71 @@ func (s3Client *mockS3Client) ListObjects(bucketName, objectPrefix string, recur
 	return result
 }
 
-//func TestDownloadFileHappyFlow(t *testing.T) {
-//	s3Config := newS3Config(&mockS3Client{}, "test-bucket", "")
-//
-//	downloadedFile, err := s3Config.downloadFile(validFileName, 2)
-//
-//	assert.Nil(t, err)
-//	assert.NotNil(t, downloadedFile)
-//}
-//
-//func TestDownloadFileWithOneRetry(t *testing.T) {
-//	initLogs(os.Stdout, os.Stdout, os.Stderr)
-//	s3Config := newS3Config(&mockS3Client{shouldRetry: true}, "test-bucket", "")
-//
-//	downloadedFile, err := s3Config.downloadFile(validFileName, 3)
-//
-//	assert.Nil(t, err)
-//	assert.NotNil(t, downloadedFile)
-//}
-//
-//func TestDownloadFileWithInvalidFileName(t *testing.T) {
-//	initLogs(os.Stdout, os.Stdout, os.Stderr)
-//	s3Config := newS3Config(&mockS3Client{}, "test-bucket", "")
-//
-//	downloadedFile, err := s3Config.downloadFile(invalidFileName, 3)
-//
-//	assert.NotNil(t, err)
-//	assert.Nil(t, downloadedFile)
-//}
-//
-//func TestUploadFileHappyFlow(t *testing.T) {
-//	initLogs(os.Stdout, os.Stdout, os.Stderr)
-//	zipFile, err := ioutil.TempFile(os.TempDir(), "test.zip")
-//	assert.Nil(t, err)
-//	tempZipName := zipFile.Name()
-//	defer os.Remove(tempZipName)
-//	zipFile.Close()
-//	s3Config := newS3Config(&mockS3Client{}, "test-bucket", "")
-//
-//	err = s3Config.uploadFile(tempZipName, "test.zip")
-//
-//	assert.Nil(t, err)
-//}
-//
-//func TestUploadFileNonExistingZip(t *testing.T) {
-//	initLogs(os.Stdout, os.Stdout, os.Stderr)
-//	s3Config := newS3Config(&mockS3Client{}, "test-bucket", "")
-//
-//	err := s3Config.uploadFile(nonExistingZip, "test.zip")
-//
-//	assert.NotNil(t, err)
-//}
-//
-//func TestUploadFileWithS3ClientFailure(t *testing.T) {
-//	initLogs(os.Stdout, os.Stdout, os.Stderr)
-//	zipFile, err := ioutil.TempFile(os.TempDir(), "test.zip")
-//	assert.Nil(t, err)
-//	tempZipName := zipFile.Name()
-//	defer os.Remove(tempZipName)
-//	zipFile.Close()
-//	s3Config := newS3Config(&mockS3Client{}, "test-bucket", "")
-//
-//	err = s3Config.uploadFile(tempZipName, invalidZipName)
-//
-//	assert.NotNil(t, err)
-//}
+func TestDownloadFileHappyFlow(t *testing.T) {
+	s3Config := newS3Config(&mockS3Client{}, "test-bucket", "")
+
+	downloadedFile, err := s3Config.downloadFile(validFileName, 2)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, downloadedFile)
+}
+
+func TestDownloadFileWithOneRetry(t *testing.T) {
+	initLogs(os.Stdout, os.Stdout, os.Stderr)
+	s3Config := newS3Config(&mockS3Client{shouldRetry: true}, "test-bucket", "")
+
+	downloadedFile, err := s3Config.downloadFile(validFileName, 3)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, downloadedFile)
+}
+
+func TestDownloadFileWithInvalidFileName(t *testing.T) {
+	initLogs(os.Stdout, os.Stdout, os.Stderr)
+	s3Config := newS3Config(&mockS3Client{}, "test-bucket", "")
+
+	downloadedFile, err := s3Config.downloadFile(invalidFileName, 3)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, downloadedFile)
+}
+
+func TestUploadFileHappyFlow(t *testing.T) {
+	initLogs(os.Stdout, os.Stdout, os.Stderr)
+	zipFile, err := ioutil.TempFile(os.TempDir(), "test.zip")
+	assert.Nil(t, err)
+	tempZipName := zipFile.Name()
+	defer os.Remove(tempZipName)
+	zipFile.Close()
+	s3Config := newS3Config(&mockS3Client{}, "test-bucket", "")
+
+	err = s3Config.uploadFile(tempZipName, "test.zip")
+
+	assert.Nil(t, err)
+}
+
+func TestUploadFileNonExistingZip(t *testing.T) {
+	initLogs(os.Stdout, os.Stdout, os.Stderr)
+	s3Config := newS3Config(&mockS3Client{}, "test-bucket", "")
+
+	err := s3Config.uploadFile(nonExistingZip, "test.zip")
+
+	assert.NotNil(t, err)
+}
+
+func TestUploadFileWithS3ClientFailure(t *testing.T) {
+	initLogs(os.Stdout, os.Stdout, os.Stderr)
+	zipFile, err := ioutil.TempFile(os.TempDir(), "test.zip")
+	assert.Nil(t, err)
+	tempZipName := zipFile.Name()
+	defer os.Remove(tempZipName)
+	zipFile.Close()
+	s3Config := newS3Config(&mockS3Client{}, "test-bucket", "")
+
+	err = s3Config.uploadFile(tempZipName, invalidZipName)
+
+	assert.NotNil(t, err)
+}
 
 func TestGetObjectKeysValidKeys(t *testing.T) {
 	initLogs(os.Stdout, os.Stdout, os.Stderr)
